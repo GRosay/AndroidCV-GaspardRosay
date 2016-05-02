bec27d8b634b90115bd40d0f3c9dc9c7838d2c46
GRosay <gaspard@rosay.ch>
2016-05-02 20:22:00 +0200

    Initial commit

---
+++ app/src/main/java/ch/gaspard_rosay/rosaygaspard/MainActivity.java
@@ -0,0 +1,84 @@
+package ch.gaspard_rosay.rosaygaspard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton mail = (FloatingActionButton) findViewById(R.id.mail);
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Sending mail to gaspard@rosay.ch...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("application/octet-stream");
+
+                String aEmailList[] = { "gaspard@rosay.ch" };
+
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
+
+                startActivity(emailIntent);
+
+            }
+        });
+        FloatingActionButton phone = (FloatingActionButton) findViewById(R.id.phone);
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Calling Gaspard Rosay...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+41791384672"));
+                try{
+                    startActivity(intent);
+                }
+                catch(Exception e){
+                    Snackbar.make(view, "Error when trying to call Gaspard Rosay!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
